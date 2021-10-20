e368e52ef7904983236b1d3b6e3a5631479b111d
Jeevan James <jeevanjj@live.com>
2021-10-20 18:58:35 +0530

    Add IProvider abstraction to providers

---
+++ provider/PostgreSql/PostgreSqlSchemaQueryProvider.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2021 Jeevan James
+// This file is licensed to you under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+
+using Npgsql;
+
+namespace Datask.Providers.PostgreSql
+{
+    public sealed class PostgreSqlSchemaQueryProvider : ISchemaQueryProvider
+    {
+        private readonly NpgsqlConnection _connection;
+
+        internal PostgreSqlSchemaQueryProvider(NpgsqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IAsyncEnumerable<TableDefinition> EnumerateTables(EnumerateTableOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ provider/Providers/IProvider.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2021 Jeevan James
+// This file is licensed to you under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Datask.Providers
+{
+    public interface IProvider : IAsyncDisposable
+    {
+        ISchemaQueryProvider SchemaQuery { get; }
+    }
+}
